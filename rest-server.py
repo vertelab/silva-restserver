3b93b59bfdbc3ca58be7f728d896a34a5abb9acc
Robin Chatfield <robin.chatfield@gmail.com>
2016-08-23 10:12:56 +0200

    Added methods for requesting order state and inventory.

--- rest-server.py
+++ rest-server.py
@@ -1,18 +1,81 @@
-from flask import Flask, url_for, request, json
+from flask import Flask, url_for, request, json, Response
 import datetime, hashlib
+
+APPKEY = "Superhemlig nyckel"
+PORT = 5000
 app = Flask(__name__)
 
-@app.route('/messages', methods = ['POST'])
-def api_message():
-
-    if request.headers['Content-Type'] == 'application/json':
-        appkey = hashlib.sha1('Min Hemliga Nyckel' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")).hexdigest()
-        if appkey == request.json['appkey']:
-            return json.dumps({'return': '200 OK', 'message': 'Hejsan'})
-        else:
-            return '400 ERROR'
+def get_inventory(article_id, amount):
+    if article_id == "1337":
+        stock = 42 
+    elif article_id == "1338":
+        stock = 13
+    elif article_id == "1336":
+        stock = 2
+    elif article_id == "1339":
+        stock = 0
     else:
-        return "415 Unsupported Media Type!"
+        stock = 0
+    print '%s\t%s' % (article_id, amount)
+    return amount if amount <= stock else stock
+
+def get_order_state(order):
+    if order == 23:
+        return "Shipped"
+    else:
+        return "Processing"
+
+def get_appkey(offset=0):
+    print (datetime.datetime.utcnow() + datetime.timedelta(seconds=offset)).strftime("%Y-%m-%d %H:%M:%S")[:-1]
+    key = hashlib.sha1(APPKEY + (datetime.datetime.utcnow() - datetime.timedelta(seconds=offset)).strftime("%Y-%m-%d %H:%M:%S")[:-1]).hexdigest()
+    print key
+    return key
+
+def check_appkey(appkey):
+    for offset in [0, -10, 10]:
+        if appkey == get_appkey(offset):
+            return True
+    return False
+
+@app.route('/inventory', methods = ['POST'])
+def api_inventory():
+    try:
+        if request.headers['Content-Type'] == 'application/json':
+            if check_appkey(request.json['appkey']):
+                result = {}
+                articles = request.json.get('articles', {})
+                for article_id in articles:
+                    result[article_id] = get_inventory(article_id, articles[article_id])
+                data = json.dumps({
+                    'appkey': get_appkey(),
+                    'inventory': result,
+                })
+                return Response(data, status=200, mimetype='application/json')
+            else:
+                return Response('500 Permission denied', status=500)
+        else:
+            return Response("415 Unsupported Media Type!", status=415)
+    except:
+        return Response('400 ERROR', status=400)
+
+@app.route('/order', methods = ['POST'])
+def api_order_state():
+    try:
+        if request.headers['Content-Type'] == 'application/json':
+            if check_appkey(request.json['appkey']):
+                order = request.json['order']
+                data = json.dumps({
+                    'appkey': get_appkey(),
+                    'order': order,
+                    'state': get_order_state(order),
+                })
+                return Response(data, status=200, mimetype='application/json')
+            else:
+                return Response('500 Permission denied', status=500)
+        else:
+            return Response("415 Unsupported Media Type!", status=415)
+    except:
+        return Response('400 ERROR', status=400)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=PORT)
