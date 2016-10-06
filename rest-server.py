3761940116b1db921ce34fddb91130f2813ff005
Robin Chatfield <robin.chatfield@gmail.com>
2016-10-06 13:25:12 +0200

    Wrote connection to MS SQL.

--- rest-server.py
+++ rest-server.py
@@ -1,55 +1,159 @@
 from flask import Flask, url_for, request, json, Response
 import datetime, hashlib
+import pymssql
 
 APPKEY = "Superhemlig nyckel"
 PORT = 5000
 HOST = "0.0.0.0"
 app = Flask(__name__)
 
-def get_inventory(article_id, amount):
-    if article_id == "1337":
-        stock = 42 
-    elif article_id == "1338":
-        stock = 13
-    elif article_id == "1336":
-        stock = 2
-    elif article_id == "1339":
-        stock = 0
-    else:
-        stock = 0
-    print '%s\t%s' % (article_id, amount)
-    return amount if amount <= stock else stock
-
-def get_order_state(order):
-    if order == 23:
-        return "Shipped"
-    else:
-        return "Processing"
+MSSQL_SERVER = 'localhost'
+MSSQL_USER = 'WebSQL'
+MSSQL_PWD = 'Web2Use!'
+MSSQL_DB = 'Silva_Test'
 
 def get_appkey(offset=0):
-    print (datetime.datetime.utcnow() + datetime.timedelta(seconds=offset)).strftime("%Y-%m-%d %H:%M:%S")[:-1]
-    key = hashlib.sha256(APPKEY + (datetime.datetime.utcnow() - datetime.timedelta(seconds=offset)).strftime("%Y-%m-%d %H:%M:%S")[:-1]).hexdigest()
-    print key
+    key = hashlib.sha256(APPKEY + (datetime.datetime.utcnow() - datetime.timedelta(minutes=offset)).strftime("%Y-%m-%d %H:%M")).hexdigest()
     return key
 
 def check_appkey(appkey):
-    for offset in [0, -10, 10]:
+    for offset in [0, -1, 1]:
         if appkey == get_appkey(offset):
             return True
     return False
+
+def py2sql(val):
+    """Prepare value(s) for use in an SQL query."""
+    if type(val) == type(()):
+        return tuple([py2sql(v) for v in val])
+    elif val == None:
+        return 'NULL'
+    elif type(val) == type(str()):
+        return "'%s'" % val
+    return val
+
+def get_inventory(articles):
+    sql="Declare @Item ISE_TblItemQty;"
+    for article_id in articles:
+        sql += "INSERT INTO @Item(ItemId,Unit,Quantity,CompanyCode) VALUES ('%s',NULL,%s,10);" % py2sql((article['ItemId'], article.get('Unit'), article['Quantity']))
+    sql += "EXEC q_ise_2web_GetItemBalance @Tbl_ItemQty = @Item;"
+    inventory = []
+    with pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            res = cursor.fetchone()
+            while res:
+                item = {
+                    'ItemId': res[0],
+                    'Unit': res[1],
+                    'Quantity': res[2],
+                    'QuantityAvailable': res[3],
+                    'CompanyCode': res[4],
+                }
+                res = cursor.fetchone()
+    return inventory
+
+def get_order_state(order):
+    headers = []
+    sql = "EXEC q_ise_2web_GetOrders @CustomerNo = %s, @OrderNo = %s, @OrderIdWeb = %s, @CompanyCode = 10;" % py2sql((order.get('CustomerNo'), order.get('OrderNo'), order.get('OrderIdWeb')))
+    with pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            res = cursor.fetchone()
+            while res:
+                head = {
+                    'CompanyCode': res[0],
+                    'OrderNo': res[1],
+                    'OrderIdWeb': res[2],
+                    'AmountExclTax': res[3],
+                    'AmountInclTax': res[4],
+                    'Currency': res[5],
+                    'StatusCode': res[6],
+                    'Status': res[7],
+                    'PaymentTermsCode': res[8],
+                    'PaymentTerms': res[9],
+                    'DeliverytypeCode': res[10],
+                    'Deliverytype': res[11],
+                    'CustomerRef': res[12],
+                    'OrderDate': res[13],
+                    'ExpDeliveryDate': res[14],
+                    'OurRef': res[15],
+                    'OrderTypeCode': res[16],
+                    'OrderType': res[17],
+                    'WebOrderNo': res[18],
+                    'CreatedDate': res[19],
+                }
+                headers.append[head]
+                res = cursor.fetchone()
+            rows = []
+            if order.get('OrderNo'):
+                sql = "EXEC q_GetOrderRows2Web  @OrderNo = %s, @CompanyCode = 10;" % py2sql(order.get('OrderNo'))
+                cursor.execute(sql)
+                res = cursor.fetchone()
+                while res:
+                    row = {
+                        'CompanyCode': res[0],
+                        'OrderRowNo': res[1],
+                        'RowIdWeb': res[2],
+                        'ItemId': res[3],
+                        'ItemDescr': res[4],
+                        'Quantity': res[5],
+                        'Unit': res[6],
+                        'AmountExltax': res[7],
+                        'TaxCode': res[8],
+                        'Tax': res[9],
+                        'DeliveredQty': res[10],
+                        'RemaningQty': res[11],
+                        'InvoicedQty': res[12],
+                        'DeliveryDate': res[13],
+                        'Discount': res[14],
+                        'Note': res[15],
+                        'StatusCode': res[16],
+                        'Status': res[17],
+                    }
+                    rows.append(row)
+                    res = cursor.fetchone()
+            return headers, rows
+
+def place_order(order):
+    head = order['head']
+    rows = order['rows']
+    sql="""DECLARE @Header ISE_TblOrderHeader;
+INSERT INTO @Header(OrderIdWeb,CustomerId,Address1,Address2,Address3,Address4,PostalCode,CountryCode,OrderReference,YourReference,OrderComment,Currency,CompanyCode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,10);
+DECLARE @Rows ISE_TblOrderRow;""" % py2mssql(
+        (head['OrderIdWeb'], head.get('CustomerId'), head.get('Address1'),
+        head.get('Address2'), head.get('Address3'), head.get('Address4'),
+        head.get('PostalCode'), head.get('CountryCode'), head.get('OrderReference'),
+        head.get('YourReference'), head.get('OrderComment'), head.get('Currency'))
+    )
+    for row in rows:
+        sql += "INSERT INTO @Rows(OrderIdWeb,RowIdWeb,ItemId,Quantity,CompanyCode) VALUES (%s,%s,%s,%s,10);" % py2sql((
+            article['OrderIdWeb'], article['RowIdWeb'], article['ItemId'], article['Quantity']))
+    sql += "EXEC q_ise_2web_GetItemBalance @Tbl_ItemQty = @Item;"
+    inventory = []
+    with pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            res = cursor.fetchone()
+            while res:
+                item = {
+                    'ItemId': res[0],
+                    'Unit': res[1],
+                    'Quantity': res[2],
+                    'QuantityAvailable': res[3],
+                    'CompanyCode': res[4],
+                }
+                res = cursor.fetchone()
+    return inventory
 
 @app.route('/inventory', methods = ['POST'])
 def api_inventory():
     try:
         if request.headers['Content-Type'] == 'application/json':
             if check_appkey(request.json['appkey']):
-                result = {}
-                articles = request.json.get('articles', {})
-                for article_id in articles:
-                    result[article_id] = get_inventory(article_id, articles[article_id])
                 data = json.dumps({
                     'appkey': get_appkey(),
-                    'inventory': result,
+                    'inventory': get_inventory(request.json.get('articles', {})),
                 })
                 return Response(data, status=200, mimetype='application/json')
             else:
@@ -59,8 +163,27 @@ def api_inventory():
     except:
         return Response('400 ERROR', status=400)
 
-@app.route('/order', methods = ['POST'])
-def api_order_state():
+@app.route('/order_info', methods = ['POST'])
+def api_order_info():
+    try:
+        if request.headers['Content-Type'] == 'application/json':
+            if check_appkey(request.json['appkey']):
+                order = request.json['order']
+                data = json.dumps({
+                    'appkey': get_appkey(),
+                    'order': order,
+                    'state': get_order_state(order),
+                })
+                return Response(data, status=200, mimetype='application/json')
+            else:
+                return Response('500 Permission denied', status=500)
+        else:
+            return Response("415 Unsupported Media Type!", status=415)
+    except:
+        return Response('400 ERROR', status=400)
+
+@app.route('/place_order', methods = ['POST'])
+def api_place_order():
     try:
         if request.headers['Content-Type'] == 'application/json':
             if check_appkey(request.json['appkey']):
