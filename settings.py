b97089c9758cfa04397450bd2f9ebbee75516a16
Robin Chatfield <robin.chatfield@gmail.com>
2016-10-07 10:33:06 +0200

    added missing files

---
+++ settings.py
@@ -0,0 +1,10 @@
+MSSQL_SERVER = 'localhost'
+MSSQL_USER = ''
+MSSQL_PWD = ''
+MSSQL_DB = ''
+MSSQL_CHARSET = 'ISO-8859-1'
+DEBUG = True
+
+APPKEY = "Superhemlig nyckel"
+PORT = 5000
+HOST = "0.0.0.0"
