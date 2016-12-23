8d99bc817fc20915a5d64d09276241d2b5037140
Robin Chatfield <robin.chatfield@vertel.se>
2016-12-23 10:27:49 +0100

    recovered lost changes

--- rest-server.py
+++ rest-server.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, url_for, request, json, Response
+from gevent.wsgi import WSGIServer
 import datetime, hashlib
 import pymssql
+import logging
+import traceback
 import sys
-from gevent.wsgi import WSGIServer
+import codecs
 
 from settings import *
 
@@ -18,118 +22,108 @@ def check_appkey(appkey):
             return True
     return False
 
-def py2sql(val):
-    """Prepare value(s) for use in an SQL query."""
-    if type(val) == type(()):
-        return tuple([py2sql(v) for v in val])
-    elif val == None:
-        return 'NULL'
-    elif type(val) in [type(str()), type(unicode())]:
-        return "'%s'" % val
-    return val
-
 def get_inventory(articles):
     sql="Declare @Item ISE_TblItemQty;"
+    data = []
     for article in articles:
-        sql += "INSERT INTO @Item(ItemId,Unit,Quantity,CompanyCode) VALUES (%s,%s,%s,10);" % py2sql((article['ItemId'], article.get('Unit'), article['Quantity']))
+        sql += "INSERT INTO @Item(ItemId,Unit,Quantity,CompanyCode) VALUES (%s,%s,%s,10);"
+        data += [article['ItemId'], article.get('Unit'), article['Quantity']]
     sql += "EXEC q_ise_2web_GetItemBalance @Tbl_ItemQty = @Item;"
     inventory = []
     conn = cursor = None
-    if DEBUG:
-        print sql
+    data = tuple(data)
+    logging.debug(sql % data)
     try:
-        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB, charset=MSSQL_CHARSET)
-        cursor = conn.cursor()
-        cursor.execute(sql)
+        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB)
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute(sql, data)
         res = cursor.fetchone()
         while res:
-            item = {
-                'ItemId': res[0],
-                'Unit': res[1],
-                'Quantity': res[2],
-                'QuantityAvailable': res[3],
-                'CompanyCode': res[4],
-            }
+            item = res
             inventory.append(item)
             res = cursor.fetchone()
     except:
+        e = sys.exc_info()
+        msg = traceback.format_exception(e[0], e[1], e[2])
+        logging.warn(''.join(msg))
         raise Warning('Failure in SQL connection.')
     finally:
         if cursor:
             cursor.close()
         if conn:
             conn.close()
-    sys.stdout.flush()
     return inventory
 
 def get_order_state(order):
     headers = []
-    sql = "EXEC q_ise_2web_GetOrders @CustomerNo = %s, @OrderNo = %s, @OrderIdWeb = %s, @CompanyCode = 10;" % py2sql((order.get('CustomerNo'), order.get('OrderNo'), order.get('OrderIdWeb')))
+    rows = []
+    sql = "EXEC q_ise_2web_GetOrders @CustomerNo = %s, @OrderNo = %s, @OrderIdWeb = %s, @CompanyCode = 10;"
+    data = (order.get('CustomerNo'), order.get('OrderNo'), order.get('OrderIdWeb'))
     conn = cursor = None
-    if DEBUG:
-        print sql
+    logging.debug(sql % data)
     try:
-        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB, charset=MSSQL_CHARSET)
+        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB)
         cursor = conn.cursor()
-        cursor.execute(sql)
+        cursor.execute(sql, data)
         res = cursor.fetchone()
         while res:
-            head = {
-                'CompanyCode': res[0],
-                'OrderNo': res[1],
-                'OrderIdWeb': res[2],
-                'AmountExclTax': res[3],
-                'AmountInclTax': res[4],
-                'Currency': res[5],
-                'StatusCode': res[6],
-                'Status': res[7],
-                'PaymentTermsCode': res[8],
-                'PaymentTerms': res[9],
-                'DeliverytypeCode': res[10],
-                'Deliverytype': res[11],
-                'CustomerRef': res[12],
-                'OrderDate': res[13],
-                'ExpDeliveryDate': res[14],
-                'OurRef': res[15],
-                'OrderTypeCode': res[16],
-                'OrderType': res[17],
-                'WebOrderNo': res[18],
-                'CreatedDate': res[19],
-            }
-            headers.append[head]
-            res = cursor.fetchone()
-        rows = []
-        if order.get('OrderNo'):
-            sql = "EXEC q_GetOrderRows2Web  @OrderNo = %s, @CompanyCode = 10;" % py2sql(order.get('OrderNo'))
-            if DEBUG:
-                print sql
-            cursor.execute(sql)
-            res = cursor.fetchone()
-            while res:
-                row = {
+            if len(res) >= 20:
+                logging.debug(res)
+                head = {
                     'CompanyCode': res[0],
-                    'OrderRowNo': res[1],
-                    'RowIdWeb': res[2],
-                    'ItemId': res[3],
-                    'ItemDescr': res[4],
-                    'Quantity': res[5],
-                    'Unit': res[6],
-                    'AmountExltax': res[7],
-                    'TaxCode': res[8],
-                    'Tax': res[9],
-                    'DeliveredQty': res[10],
-                    'RemaningQty': res[11],
-                    'InvoicedQty': res[12],
-                    'DeliveryDate': res[13],
-                    'Discount': res[14],
-                    'Note': res[15],
-                    'StatusCode': res[16],
-                    'Status': res[17],
+                    'OrderNo': res[1],
+                    'OrderIdWeb': res[2],
+                    'ExternalOrderId': res[3],
+                    'AmountExclTax': res[4],
+                    'AmountInclTax': res[5],
+                    'Currency': res[6],
+                    'StatusCode': res[7],
+                    'Status': res[8],
+                    'PaymentTermsCode': res[9],
+                    'PaymentTerms': res[10],
+                    'DeliverytypeCode': res[11],
+                    'Deliverytype': res[12],
+                    'CustomerRef': res[13],
+                    'OrderDate': res[14],
+                    'ExpDeliveryDate': res[15],
+                    'OurRef': res[16],
+                    'OrderTypeCode': res[17],
+                    'OrderType': res[18],
+                    'CreatedDate': res[19], 
                 }
-                rows.append(row)
-                res = cursor.fetchone()
+                headers.append(head)
+                if order.get('OrderNo') or head.get('OrderNo'):
+                    sql = "EXEC q_ise_2web_GetOrderRows  @OrderNo = %s;"
+                    data = (order.get('OrderNo') or head.get('OrderNo'))
+                    logging.debug(sql % data)
+                    cursor.execute(sql, data)
+                    res = cursor.fetchone()
+                    while res:
+                        logging.debug(res)
+                        row = {
+                            'CompanyCode': res[0],
+                            'OrderRowNo': res[1],
+                            'RowIdWeb': res[2],
+                            'ItemId': res[3],
+                            'ItemDescr': res[4],
+                            'Quantity': res[5],
+                            'Unit': res[6],
+                            'AmountExltax': res[7],
+                            'TaxCode': res[8],
+                            'Tax': res[9],
+                            'DeliveredQty': res[10],
+                            'RemaningQty': res[11],
+                            'InvoicedQty': res[12],
+                            'DeliveryDate': res[13],
+                            'Discount': res[14],
+                            'Note': res[15],
+                            'StatusCode': res[16],
+                            'Status': res[17],
+                        }
+                        rows.append(row)
+                        res = cursor.fetchone()
     except:
-        raise Warning('Failure in SQL connection.')
+            raise Warning('Failure in SQL connection.')
     finally:
         if cursor:
             cursor.close()
@@ -142,42 +136,50 @@ def place_order(order):
     head = order['head']
     rows = order['rows']
     sql="""DECLARE @Header ISE_TblOrderHeader;
-INSERT INTO @Header(OrderIdWeb,CustomerId,Address1,Address2,Address3,Address4,PostalCode,CountryCode,OrderReference,YourReference,OrderComment,Currency,CompanyCode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,10);
-DECLARE @Rows ISE_TblOrderRow;""" % py2sql(
-        (head['OrderIdWeb'], head.get('CustomerId'), head.get('Address1'),
+INSERT INTO @Header(OrderIdWeb,CustomerId,Address1,Address2,Address3,Address4,PostalCode,CountryCode,OrderReference,YourReference,OrderComment,Currency,CompanyCode, Contact, Extra1, Extra2, Extra3) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,10, %s, %s, %s, %s);
+DECLARE @Rows ISE_TblOrderRow;"""
+    data = [head['OrderIdWeb'], head.get('CustomerId'), head.get('Address1'),
         head.get('Address2'), head.get('Address3'), head.get('Address4'),
         head.get('PostalCode'), head.get('CountryCode'), head.get('OrderReference'),
-        head.get('YourReference'), head.get('OrderComment'), head.get('Currency'))
-    )
+        head.get('YourReference'), head.get('OrderComment'), head.get('Currency'),
+        head.get('Contact'), head.get('Extra1'), head.get('Extra2'), head.get('Extra3')]
     for row in rows:
-        sql += "INSERT INTO @Rows(OrderIdWeb,RowIdWeb,ItemId,Quantity,CompanyCode) VALUES (%s,%s,%s,%s,10);" % py2sql((
-            head['OrderIdWeb'], row['RowIdWeb'], row['ItemId'], row['Quantity']))
+        sql += "INSERT INTO @Rows(OrderIdWeb,RowIdWeb,ItemId,Quantity,CompanyCode, Extra1, Extra2, Extra3) VALUES (%s,%s,%s,%s,10, %s, %s, %s);"
+        data += [head['OrderIdWeb'], row['RowIdWeb'], row['ItemId'], row['Quantity'],
+            row.get('Extra1'), row.get('Extra2'), row.get('Extra3')]
     sql += "EXEC q_ISE_Web_IntegrateOrder @tblOrderHeader = @Header, @tblOrderRow = @Rows;"
     conn = cursor = None
-    res = "SQL Error"
-    if DEBUG:
-        print sql
+    result = "SQL Error"
+    logging.debug(sql % data)
     try:
-        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB, charset=MSSQL_CHARSET)
+        conn = pymssql.connect(host=MSSQL_SERVER, user=MSSQL_USER, password=MSSQL_PWD, database=MSSQL_DB)
         cursor = conn.cursor()
-        cursor.execute(sql)
-        res = cursor.fetchone()[0]
-        if res == ' ':
+        cursor.execute(sql, data)
+        try:
             conn.commit()
+            result = ''
+        except:
+            e = sys.exc_info()
+            msg = traceback.format_exception(e[0], e[1], e[2])
+            logging.warn(''.join(msg))
+            result = u'Fel vid orderläggning!'
     except:
-        raise Warning('Failure in SQL connection.')
+        e = sys.exc_info()
+        msg = traceback.format_exception(e[0], e[1], e[2])
+        logging.warn(''.join(msg))
     finally:
         if cursor:
             cursor.close()
         if conn:
             conn.close()
-    sys.stdout.flush()
-    return res
+    logging.debug(result)
+    return result
 
 @app.route('/inventory', methods = ['POST'])
 def api_inventory():
     try:
         if request.headers['Content-Type'] == 'application/json':
+            logging.debug(request.json)
             if check_appkey(request.json['appkey']):
                 data = json.dumps({
                     'appkey': get_appkey(),
@@ -189,12 +191,16 @@ def api_inventory():
         else:
             return Response("415 Unsupported Media Type!", status=415)
     except:
+        e = sys.exc_info()
+        msg = traceback.format_exception(e[0], e[1], e[2])
+        logging.warn(''.join(msg))
         return Response('400 ERROR', status=400)
 
 @app.route('/order_info', methods = ['POST'])
 def api_order_info():
     try:
         if request.headers['Content-Type'] == 'application/json':
+            logging.debug(request.json)
             if check_appkey(request.json['appkey']):
                 order = request.json['order']
                 head, rows = get_order_state(order)
@@ -209,12 +215,16 @@ def api_order_info():
         else:
             return Response("415 Unsupported Media Type!", status=415)
     except:
+        e = sys.exc_info()
+        msg = traceback.format_exception(e[0], e[1], e[2])
+        logging.warn(''.join(msg))
         return Response('400 ERROR', status=400)
 
 @app.route('/place_order', methods = ['POST'])
 def api_place_order():
     try:
         if request.headers['Content-Type'] == 'application/json':
+            logging.debug(request.json)
             if check_appkey(request.json['appkey']):
                 order = request.json['order']
                 data = json.dumps({
@@ -228,7 +238,11 @@ def api_place_order():
         else:
             return Response("415 Unsupported Media Type!", status=415)
     except:
+        e = sys.exc_info()
+        msg = traceback.format_exception(e[0], e[1], e[2])
+        logging.warn(''.join(msg))
         return Response('400 ERROR', status=400)
+        
 
 http_server = WSGIServer((HOST, PORT), app)
 http_server.serve_forever()
